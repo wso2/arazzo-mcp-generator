8a4c8a2d28f99641a5b8ac69eb20c08886f63c19
HimethW <himethkbw@gmail.com>
2026-04-18 21:44:34 +0530

    fix(generate): add port validation to ensure valid range for port numbers

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package cmd
 
 import (
@@ -74,6 +92,11 @@ func init() {
 }
 
 func runGenerateCommand() error {
+	// Validate port range
+	if generatePort < 1 || generatePort > 65535 {
+		return fmt.Errorf("invalid port %d: must be between 1 and 65535", generatePort)
+	}
+
 	// Validate flag usage: exactly one of --folder or --file must be provided
 	if generateFolder == "" && generateFile == "" {
 		return fmt.Errorf("either --folder (-d) or --file (-f) must be specified\n\n" +
